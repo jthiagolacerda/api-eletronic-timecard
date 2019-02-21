1ad9a78e294f393347371ced60120e0541b5abf9
Jésus Thiago <jesus.lacerda@vistajet.com>
2019-02-21 00:23:26 -0300

    first commit

---
+++ src/main/java/com/jt/lab/labeletronictimecard/LabEletronicTimecardApplication.java
@@ -0,0 +1,13 @@
+package com.jt.lab.labeletronictimecard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabEletronicTimecardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabEletronicTimecardApplication.class, args);
+	}
+
+}
